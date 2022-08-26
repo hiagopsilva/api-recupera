22ced68490fe7cf684e39a7435b1f02c981c240e
Hiago Prates <hiagohps360@gmail.com>
2022-08-26 00:37:29 -0300

    chore: update port

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(3333, () => {
-  console.log('Server started in port: 3333')
+const port = process.env.PORT || 3333
+
+app.listen(port, () => {
+  console.log(`Server started in port: ${port}`)
 })
