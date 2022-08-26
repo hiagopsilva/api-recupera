d165d414515bcb1cb69484806a045d0eecfe8ab3
Hiago Prates <hiagohps360@gmail.com>
2022-08-26 01:28:00 -0300

    chore: update

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -2,48 +2,64 @@ import api from '../services'
 
 class UsersController {
   async auth (request, response) {
-    const dataAuth = await api.post('/auth', {
-      user: request.body.user,
-      pass: request.body.password
-    })
+    try {
+      const dataAuth = await api.post('/auth', {
+        user: request.body.user,
+        pass: request.body.password
+      })
 
-    return response.json({ data: dataAuth.data.token })
+      return response.json({ data: dataAuth.data.token })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   async debtors (request, response) {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${request.headers.token}`
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${request.headers.token}`
+        }
       }
+
+      const { data } = await api.get('/debtors/list', config)
+
+      return response.json(data)
+    } catch (error) {
+      console.log(error)
     }
-
-    const { data } = await api.get('/debtors/list', config)
-
-    return response.json(data)
   }
 
   async debts (request, response) {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${request.headers.token}`
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${request.headers.token}`
+        }
       }
+
+      const { data } = await api.get('/debts/list', config)
+
+      return response.json(data)
+    } catch (error) {
+      console.log(error)
     }
-
-    const { data } = await api.get('/debts/list', config)
-
-    return response.json(data)
   }
 
   async agreement (request, response) {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${request.headers.token}`
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${request.headers.token}`
+        }
       }
+
+      const { data } = await api.get('/agreements/list', config)
+
+      return response.json(data)
+    } catch (error) {
+      console.log(error)
     }
-
-    const { data } = await api.get('/agreements/list', config)
-
-    return response.json(data)
   }
 }
 
